ae15665de03939f8e322a06bb2cd8da6b24e796a
Gerry Putra <gerryputra.gry@gmail.com>
2023-10-10 02:23:29 -0700

    change static default path to serve images in file system

--- application/backend/__init__.py
+++ application/backend/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 db = SQLAlchemy()
 
 def create_app():
-    app = Flask(__name__, template_folder="../frontend/templates")
+    app = Flask(__name__, template_folder="../frontend/templates", static_folder="../frontend/static")
 
     # Database configuration
     app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:oldschool@localhost/biteRateDB'
