c174cd47f6860b4abe819c028f51a46366a05553
Gerry Putra <gerryputra.gry@gmail.com>
2023-10-11 23:23:17 -0700

    change URI from local to remote one

--- application/backend/__init__.py
+++ application/backend/__init__.py
@@ -9,7 +9,7 @@ def create_app():
     app.secret_key = 'bite_rate_app session_config secret_key'
 
     # Database configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:oldschool@localhost/biteRateDB'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:oldschool@34.82.27.119/biteRateDB'
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking to save resources
 
     db.init_app(app)
