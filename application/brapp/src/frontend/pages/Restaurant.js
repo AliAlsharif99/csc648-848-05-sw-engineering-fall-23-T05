a72848f7d8e668f41a3c058dc3bfe8d196b57501
Robin Reyes <robinreyes.code@gmail.com>
2023-10-31 12:10:24 -0700

    added review ui

--- application/brapp/src/frontend/pages/Restaurant.js
+++ application/brapp/src/frontend/pages/Restaurant.js
@@ -1,6 +1,22 @@
 import React from "react";
 import pic from "../images/ramenHouse.jpg";
+import x from "../images/x.png";
 import ramen from "../images/ramen.jpg";
+
+function toggleReview() {
+  const elem = document.querySelector(".review-ui");
+  elem.classList.toggle("show");
+}
+
+function toggleThankYou() {
+  const head = document.querySelector(".review-header");
+  const text = document.querySelector(".review-textarea");
+  const btn = document.querySelector(".review-btn");
+  head.classList.add("hide");
+  text.classList.add("hide");
+  btn.classList.add("hide");
+}
+
 function Restaurant() {
   // restaurant info
   const restaurantName = <p className="rest-name">Fuki Ramen</p>;
@@ -18,7 +34,30 @@ function Restaurant() {
     </ul>
   );
 
-  const reviewButton = <button className="btn">Leave a Review!</button>;
+  const reviewButton = (
+    <button className="btn" onClick={toggleReview}>
+      Leave a Review!
+    </button>
+  );
+
+  const reviewUI = (
+    <>
+      <div className="review-ui">
+        <p className="review-header">Leave your review for Fuki Ramen!</p>
+        <textarea className="review-textarea" rows="12" cols="60"></textarea>
+        <button
+          className="review-btn"
+          onClick={toggleThankYou}
+          type="submit"
+          width="100px"
+        >
+          submit
+        </button>
+        <p className="thanks">Thank you!</p>
+        <img src={x} height="20px" className="x" onClick={toggleReview}></img>
+      </div>
+    </>
+  );
 
   const score1 = <p className="score">7.7</p>;
   const score2 = <p className="score">6.1</p>;
@@ -72,6 +111,8 @@ function Restaurant() {
           </div>
         </div>
       </div>
+
+      {reviewUI}
     </>
   );
 }
