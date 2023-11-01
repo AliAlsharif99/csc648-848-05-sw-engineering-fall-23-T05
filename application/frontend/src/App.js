5b3cd0566143ded7c87596fa14a5ecae5902baed
mil3aga <agk.alsharif@gmail.com>
2023-10-31 22:03:27 -0700

    added login and fixed paths

--- application/frontend/src/App.js
+++ application/frontend/src/App.js
@@ -1,14 +1,15 @@
 import "./App.css";
-import Navbar from "./frontend/Navbar";
-import Home from "./frontend/pages/Home";
-import Leaderboard from "./frontend/pages/Leaderboard";
-import MeetTheTeam from "./frontend/pages/MeetTheTeam";
-import Search from "./frontend/pages/Search";
-import Profile from "./frontend/pages/Profile";
-import EditProfile from "./frontend/pages/EditProfile";
-import SignUp from "./frontend/pages/SignUp";
-import Lists from "./frontend/pages/Lists";
-import Restaurant from "./frontend/pages/Restaurant";
+import Navbar from "./resources/Navbar";
+import Home from "./resources/pages/Home";
+import Leaderboard from "./resources/pages/Leaderboard";
+import MeetTheTeam from "./resources/pages/MeetTheTeam";
+import Search from "./resources/pages/Search";
+import Profile from "./resources/pages/Profile";
+import EditProfile from "./resources/pages/EditProfile";
+import SignUp from "./resources/pages/SignUp";
+import Login from "./resources/pages/Login";
+import Lists from "./resources/pages/Lists";
+import Restaurant from "./resources/pages/Restaurant";
 import { Route, Routes } from "react-router-dom";
 
 function App() {
@@ -25,6 +26,7 @@ function App() {
           <Route path="/profile" element={<Profile />} />
           <Route path="/edit-profile" element={<EditProfile />} />
           <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
           <Route path="/lists" element={<Lists />} />
           <Route path="/restaurant" element={<Restaurant />}></Route>
         </Routes>
