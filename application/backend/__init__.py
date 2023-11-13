ebe04011e2c540d51be5425190d5d6545d2e92d9
Gerry Putra <gerryputra.gry@gmail.com>
2023-11-13 04:30:48 -0800

    set up env variables to store sensitive data; set up python-dotenv

--- application/backend/__init__.py
+++ application/backend/__init__.py
@@ -4,25 +4,29 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 from flask_caching import Cache
 from flask_session import Session
+from dotenv import load_dotenv
+
 
 db = SQLAlchemy()
 cache = Cache(config={'CACHE_TYPE': 'simple', 'CACHE_DEFAULT_TIMEOUT': 300})  # Configure cache
+load_dotenv()  # Loads env. var. from .env file
 
 
 def create_app():
     app = Flask(__name__)
-    app.secret_key = 'bite_rate_app session_config secret_key'
+    app.secret_key = os.environ.get('SECRET_KEY', 'default_secret_key')
 
-    # Cross-Origin Resource Sharing (CORS) : With this, will allow our React app to make requests to our Flask server
+    # Cross-Origin Resource Sharing (CORS) configuration
     if os.environ.get('FLASK_ENV') == 'production':
-        cors_origins = "http://biterate-env-1.eba-r3mebmuv.us-east-2.elasticbeanstalk.com"
+        cors_origins = os.environ.get('PROD_CORS_ORIGINS', 'default_production_origin')
     else:
-        cors_origins = "http://localhost:3000"
+        cors_origins = os.environ.get('DEV_CORS_ORIGINS', 'http://localhost:3000')
     CORS(app, resources={r"/api/*": {"origins": cors_origins}}, supports_credentials=True)
 
-    # Database configuration
-    app.config['SQLALCHEMY_DATABASE_URI'] \
-        = 'mysql+pymysql://admin:oldschool@biteratedb-proto.cnoyllr6qcmr.us-east-2.rds.amazonaws.com:3306/biteRateDB'
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+        'SQLALCHEMY_DATABASE_URI',
+        'default_database_uri'
+    )
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking to save resources
 
     # Flask-Session configuration
