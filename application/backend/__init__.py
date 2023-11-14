2db342f4975e67fdb43599f653b55c018bfaac53
Gerry Putra <gerryputra.gry@gmail.com>
2023-11-13 22:04:18 -0800

    add production url

--- application/backend/__init__.py
+++ application/backend/__init__.py
@@ -30,6 +30,7 @@ def create_app():
     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Disable tracking to save resources
 
     # Flask-Session configuration
+    app.config['SESSION_PERMANENT'] = False
     app.config['SESSION_TYPE'] = 'filesystem'
     app.config['SESSION_COOKIE_NAME'] = 'session'
     app.config['SESSION_COOKIE_PATH'] = '/'
